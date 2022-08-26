28a1a3320bee3d4649c0f7de3b12948c869172ba
domlimm <dominiclimkj3448@gmail.com>
2022-08-26 14:46:01 +0800

    chore: update semi colon to true

--- user-service/src/controller/user-controller.ts
+++ user-service/src/controller/user-controller.ts
@@ -1,31 +1,31 @@
-import { Request, Response } from 'express'
+import { Request, Response } from 'express';
 import {
   ormCreateUser as _createUser,
   ormGetUserInfoFromToken as _getUserInfoFromToken,
   ormGetVerifiedUserAndToken as _getVerifiedUserAndToken,
-} from '../model/user-orm.js'
-import { errorHandler } from '../utils/errorHandler.js'
-import { HttpStatusCode } from '../utils/HttpsStatusCode.js'
+} from '../model/user-orm.js';
+import { errorHandler } from '../utils/errorHandler.js';
+import { HttpStatusCode } from '../utils/HttpsStatusCode.js';
 
 export const createUser = async (req: Request, res: Response) => {
   try {
-    const { username, password } = req.body
+    const { username, password } = req.body;
 
-    await _createUser(username, password)
-    res.sendStatus(HttpStatusCode.OK)
+    await _createUser(username, password);
+    res.sendStatus(HttpStatusCode.OK);
   } catch (error) {
-    errorHandler(res, error)
+    errorHandler(res, error);
   }
-}
+};
 
 export const logIn = async (req: Request, res: Response) => {
   try {
-    const { username, password } = req.body
+    const { username, password } = req.body;
 
     const { verifiedUser, token } = await _getVerifiedUserAndToken(
       username,
       password
-    )
+    );
 
     res
       .cookie('token', token, {
@@ -35,24 +35,24 @@ export const logIn = async (req: Request, res: Response) => {
         maxAge: 1000 * 60 * 60 * 24,
       })
       .send(HttpStatusCode.OK)
-      .json(verifiedUser)
+      .json(verifiedUser);
   } catch (error) {
-    errorHandler(res, error)
+    errorHandler(res, error);
   }
-}
+};
 
 export const logOut = async (_: Request, res: Response) => {
   try {
-    res.clearCookie('token').sendStatus(HttpStatusCode.OK)
+    res.clearCookie('token').sendStatus(HttpStatusCode.OK);
   } catch (error) {
-    errorHandler(res, error)
+    errorHandler(res, error);
   }
-}
+};
 
 export const getUserInfoFromToken = async (req: Request, res: Response) => {
-  const { userId } = req.body
+  const { userId } = req.body;
 
-  const userInfo = await _getUserInfoFromToken(userId)
+  const userInfo = await _getUserInfoFromToken(userId);
 
-  res.send(HttpStatusCode.OK).json(userInfo)
-}
+  res.send(HttpStatusCode.OK).json(userInfo);
+};
