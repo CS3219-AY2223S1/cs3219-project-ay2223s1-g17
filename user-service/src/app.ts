841e2748f5d3fccf5661fa7cd13758d63a1e621e
myc37 <nvjn37@gmail.com>
2022-08-26 14:20:38 +0800

    feat: set up express app

---
+++ user-service/src/app.ts
@@ -0,0 +1,37 @@
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import 'dotenv/config'
+import express, { Express } from 'express'
+
+const app: Express = express()
+const port = process.env.PORT || '8001'
+
+// middleware
+const allowedOrigins = ['http://localhost:3000']
+app.use(
+  cors({
+    credentials: true,
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true)
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          'The CORS policy for this site does not ' +
+          'allow access from the specified Origin.'
+        return callback(new Error(msg), false)
+      }
+      return callback(null, true)
+    },
+  })
+)
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
+})
