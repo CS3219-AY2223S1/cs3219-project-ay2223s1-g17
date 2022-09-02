9a1c342aed68a343c67db0cbe41fdcddfa7c0416
myc37 <nvjn37@gmail.com>
2022-09-02 17:08:34 +0800

    chore: refactor routes to align with changes to user controller

--- user-service/src/routes/index.ts
+++ user-service/src/routes/index.ts
@@ -1,18 +1,24 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 import {
-  createUser,
-  getUserInfoFromToken,
-  logIn,
-  logOut,
-} from '../controller/user-controller';
-import { authenticateUser } from '../middleware/authenticateUser';
+  fetchFriendsListByToken,
+  login,
+  logout,
+  refreshUserInfoByToken,
+  register,
+} from '../controller';
+import { authenticate } from '../middleware';
+import { HttpStatusCode } from '../utils';
 
 const router = Router();
 
+router.get('/', (_: Request, res: Response) => {
+  res.status(HttpStatusCode.OK).send('<h1>User Service</h1>');
+});
 // user service routes
-router.route('/create').post(createUser);
-router.route('/log-in').post(logIn);
-router.route('/log-out').post(authenticateUser, logOut);
-router.route('/info').get(authenticateUser, getUserInfoFromToken);
+router.route('/register').post(register);
+router.route('/login').post(login);
+router.route('/logout').post(authenticate, logout);
+router.route('/refresh').post(authenticate, refreshUserInfoByToken);
+router.route('/friends').post(authenticate, fetchFriendsListByToken);
 
 export default router;
