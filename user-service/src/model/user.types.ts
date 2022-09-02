83817d4d5011d9f1215d346451c7c072b51d658b
myc37 <nvjn37@gmail.com>
2022-09-02 17:06:40 +0800

    feat!: refactor user model to remove unnecessary layers in mvc pattern

---
+++ user-service/src/model/user.types.ts
@@ -0,0 +1,31 @@
+import { PopulatedDoc, Types, Model, Document } from 'mongoose';
+
+export interface IUser {
+  username: string;
+  password: string;
+  friends: PopulatedDoc<IUser>[];
+}
+
+export type UserDocument = Document<unknown, any, IUser> &
+  IUser & {
+    _id: Types.ObjectId;
+  } & IUserMethods;
+
+export interface IUserMethods {
+  generateJwtToken(): string;
+}
+
+export type UserQueryOptions = {
+  onlySelectId?: boolean;
+  onlySelectFriends?: boolean;
+};
+
+export interface IUserModel extends Model<IUser, {}, IUserMethods> {
+  createUser(username: string, password: string): Promise<void>;
+  findVerifiedUser(username: string, password: string): Promise<UserDocument>;
+  findUserByUsername(
+    username: string,
+    options?: UserQueryOptions
+  ): Promise<UserDocument>;
+  findUserById(id: string, options?: UserQueryOptions): Promise<UserDocument>;
+}
