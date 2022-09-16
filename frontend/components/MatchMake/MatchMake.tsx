449c74cd10594663e275b28dd948a2f834768d84
myc37 <nvjn37@gmail.com>
2022-09-16 13:52:05 +0800

    feat: sync match making with countdown

--- frontend/components/MatchMake/MatchMake.tsx
+++ frontend/components/MatchMake/MatchMake.tsx
@@ -1,109 +1,82 @@
 import useSocket from 'components/MatchMake/hooks/useSocket';
-import { Box, Button, Stack, Typography } from '@mui/material/';
+import { Button, Stack, Typography } from '@mui/material/';
 import { DIFFICULTY } from 'utils/enums';
+import Countdown from 'components/Countdown';
 
 const MatchMake = () => {
-  const { isConnected, startMatch, isMatching, count } = useSocket();
+  const { startMatch, isMatching, count } = useSocket();
 
-  return (
-    <Box
+  return isMatching && count ? (
+    <Countdown count={count} />
+  ) : (
+    <Stack
       sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
+        borderRadius: '13px',
+        width: '55vh',
+        bgcolor: 'white',
         alignItems: 'center',
-        height: '100vh',
-        backgroundImage: 'linear-gradient(to right, #3275c4, #1c2d50)',
+        columnGap: 8,
+        paddingY: 4,
+        paddingX: 8,
       }}
     >
-      <Typography
-        variant="h5"
-        align="center"
-        color="white"
-        fontWeight="bold"
-        sx={{ marginBottom: 4 }}
-      >
-        Connected: {'' + isConnected}
-      </Typography>
       <Stack
+        spacing={2}
         sx={{
-          borderRadius: '13px',
-          width: '55vh',
-          bgcolor: 'white',
-          alignItems: 'center',
-          columnGap: 8,
-          paddingY: 4,
-          paddingX: 8,
+          width: '40vh',
         }}
       >
-        <Stack
-          spacing={2}
-          sx={{
-            width: '40vh',
+        <Typography variant="h5" align="center" color="black" fontWeight="bold">
+          Select Difficulty
+        </Typography>
+        <p></p>
+        <Button
+          type="submit"
+          variant="contained"
+          size="large"
+          style={{
+            backgroundColor: '#93DB9A',
+            fontSize: '20px',
+            fontWeight: 'bold',
+            textTransform: 'none',
           }}
+          disabled={isMatching}
+          onClick={() => startMatch(DIFFICULTY.EASY)}
         >
-          <Typography
-            variant="h5"
-            align="center"
-            color="black"
-            fontWeight="bold"
-          >
-            Select Difficulty
-          </Typography>
-          <p></p>
-          <Button
-            type="submit"
-            variant="contained"
-            size="large"
-            style={{
-              backgroundColor: '#93DB9A',
-              fontSize: '20px',
-              fontWeight: 'bold',
-              textTransform: 'none',
-            }}
-            disabled={isMatching}
-            onClick={() => startMatch(DIFFICULTY.EASY)}
-          >
-            {DIFFICULTY.EASY}
-          </Button>
-          <Button
-            type="submit"
-            variant="contained"
-            size="large"
-            style={{
-              backgroundColor: '#F8B06E',
-              fontSize: '20px',
-              fontWeight: 'bold',
-              textTransform: 'none',
-            }}
-            disabled={isMatching}
-            onClick={() => startMatch(DIFFICULTY.MEDIUM)}
-          >
-            {DIFFICULTY.MEDIUM}
-          </Button>
-          <Button
-            type="submit"
-            variant="contained"
-            size="large"
-            style={{
-              backgroundColor: '#ED8D8D',
-              fontSize: '20px',
-              fontWeight: 'bold',
-              textTransform: 'none',
-            }}
-            disabled={isMatching}
-            onClick={() => startMatch(DIFFICULTY.HARD)}
-          >
-            {DIFFICULTY.HARD}
-          </Button>
-          {count !== null && (
-            <div>
-              <b>{count}</b>
-            </div>
-          )}
-        </Stack>
+          {DIFFICULTY.EASY}
+        </Button>
+        <Button
+          type="submit"
+          variant="contained"
+          size="large"
+          style={{
+            backgroundColor: '#F8B06E',
+            fontSize: '20px',
+            fontWeight: 'bold',
+            textTransform: 'none',
+          }}
+          disabled={isMatching}
+          onClick={() => startMatch(DIFFICULTY.MEDIUM)}
+        >
+          {DIFFICULTY.MEDIUM}
+        </Button>
+        <Button
+          type="submit"
+          variant="contained"
+          size="large"
+          style={{
+            backgroundColor: '#ED8D8D',
+            fontSize: '20px',
+            fontWeight: 'bold',
+            textTransform: 'none',
+          }}
+          disabled={isMatching}
+          onClick={() => startMatch(DIFFICULTY.HARD)}
+        >
+          {DIFFICULTY.HARD}
+        </Button>
       </Stack>
-    </Box>
+    </Stack>
   );
 };
 
