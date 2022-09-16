0d3bd86906e8d3de8eaf3acce26c3f32babe8d18
domlimm <dominiclimkj3448@gmail.com>
2022-09-16 12:22:04 +0800

    feat: add functionality to get random question based on difficulty

---
+++ backend/question-service/src/model/question.model.ts
@@ -0,0 +1,71 @@
+import mongoose from 'mongoose';
+import { IQuestion, IQuestionModel } from './question.type';
+
+const questionSchema = new mongoose.Schema<IQuestion, IQuestionModel>({
+  title: {
+    type: String,
+    required: true,
+  },
+  difficulty: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  examples: {
+    type: [
+      {
+        input: {
+          type: String,
+          required: true,
+        },
+        output: {
+          type: String,
+          required: true,
+        },
+        explanation: {
+          type: String,
+        },
+      },
+    ],
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+});
+
+questionSchema.static(
+  'findQuestionByDifficulty',
+  /**
+   * Attempts to find a Question randomly based on difficulty specified
+   *
+   * @param difficulty Difficulty of a Question
+   */
+  async (difficulty: string) => {
+    if (!difficulty) throw new Error('Difficulty is required');
+
+    const query = {
+      difficulty: difficulty,
+    };
+
+    const count = await Question.countDocuments(query);
+
+    if (count === 0) throw new Error('No Questions available, seed questions');
+
+    const randomSkip = Math.floor(Math.random() * count);
+
+    const question = await Question.findOne().skip(randomSkip);
+
+    return question;
+  }
+);
+
+const Question = mongoose.model<IQuestion, IQuestionModel>(
+  'Question',
+  questionSchema
+);
+
+export default Question;
