c82d0c2f96d97e3b4df93b1e66cea6ec05b306be
myc37 <nvjn37@gmail.com>
2022-09-18 11:39:17 +0800

    style: improve ui for landing page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,15 +1,32 @@
+import { Stack } from '@mui/material';
+import PageWrapper from 'components/PageWrapper';
+import useAuth from 'contexts/useAuth';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
 const Home: NextPage = () => {
+  const { user } = useAuth();
+  console.log(user);
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>PeerPrep</title>
         <meta name="PeerPrep" content="Leetcode with friends uwu" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <p>Welcome to PeerPrep!</p>
+      <PageWrapper>
+        <Stack
+          sx={{
+            height: '100%',
+            color: 'black',
+            textAlign: 'center',
+            paddingTop: '30%',
+            fontSize: 24,
+          }}
+        >
+          {`Welcome to Peer Prep${user ? `, ${user.username}` : ''}`}
+        </Stack>
+      </PageWrapper>
     </div>
   );
 };
