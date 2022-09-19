879d2db116aefa98fd985f1aa01c54ef6f059190
domlimm <dominiclimkj3448@gmail.com>
2022-09-19 21:20:04 +0800

    fix: update clashing routes

--- backend/question-service/src/routes/index.ts
+++ backend/question-service/src/routes/index.ts
@@ -11,7 +11,7 @@ router.get('/', (_: Request, res: Response) => {
   res.status(200).send('<h1>Question Service</h1>');
 });
 
-router.route('/get/:id').get(getQuestion);
+router.route('/:id').get(getQuestion);
 
 router.route('/get/:difficulty').get(getQuestionByDifficulty);
 
