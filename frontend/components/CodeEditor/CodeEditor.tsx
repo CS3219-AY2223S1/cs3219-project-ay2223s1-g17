b724e43a91ec7b0c70b704266104ae81b667d34f
myc37 <nvjn37@gmail.com>
2022-09-21 21:12:44 +0800

    feat: ui/ux improvements to code editor
    - add views
    - hybrid view is resizable
    - editor works with dynamic values

--- frontend/components/CodeEditor/CodeEditor.tsx
+++ frontend/components/CodeEditor/CodeEditor.tsx
@@ -1,125 +1,259 @@
-import Editor from "@monaco-editor/react";
-import { Box, Divider, Stack, Typography } from "@mui/material";
-import { DIFFICULTY } from "util/enums";
+import Editor from '@monaco-editor/react';
+import {
+  Box,
+  Divider,
+  MenuItem,
+  Select,
+  Stack,
+  ToggleButton,
+  ToggleButtonGroup,
+  Typography,
+} from '@mui/material';
+import { FC, useState, ReactNode } from 'react';
+import { DIFFICULTY, LANGUAGE, VIEW } from 'utils/enums';
+import DescriptionIcon from '@mui/icons-material/Description';
+import VerticalSplitIcon from '@mui/icons-material/VerticalSplit';
+import CodeIcon from '@mui/icons-material/Code';
 
-const difficultyColors = {
-  [DIFFICULTY.EASY]: '#93DB9A',
-  [DIFFICULTY.MEDIUM]: '#F8B06E',
-  [DIFFICULTY.HARD]: '#ED8D8D',
-}
+const ViewButtonMap: Record<VIEW, ReactNode> = {
+  QUESTION: <DescriptionIcon />,
+  HYBRID: <VerticalSplitIcon sx={{ transform: 'rotate(180deg)' }} />,
+  EDITOR: <CodeIcon />,
+};
 
-const CodeEditor = () => {
-  const difficulty = DIFFICULTY.EASY
+const DifficultyColorMap: Record<DIFFICULTY, string> = {
+  EASY: '#93DB9A',
+  MEDIUM: '#F8B06E',
+  HARD: '#ED8D8D',
+};
 
+export type Question = {
+  title: string;
+  difficulty: DIFFICULTY;
+  description: string;
+  examples: { input: string; output: string; explanation: string }[];
+  constraints: string[];
+  templates: Record<LANGUAGE, string>;
+};
+
+const CodeEditor: FC<Question> = ({
+  title,
+  difficulty,
+  description,
+  examples,
+  constraints,
+  templates,
+}) => {
+  const [language, setLanguage] = useState<LANGUAGE>(LANGUAGE.PYTHON);
+  const [view, setView] = useState<VIEW>(VIEW.HYBRID);
+  const [editorContent, setEditorContent] = useState(
+    templates[language] ?? '// start coding here'
+  );
 
   return (
     <Box
       sx={{
+        height: '100%',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        height: '100vh',
-        backgroundImage: 'linear-gradient(to right, #3275c4, #1c2d50)',
+        width: '100%',
+        overflowX: 'hidden',
+        flexDirection: 'column',
+        pb: 2,
       }}
     >
       <Stack
-        borderRadius="20px"
-        direction="row"
-        divider={<Divider orientation="vertical" flexItem />}
+        sx={{ width: '100%', px: 4, py: 2 }}
+        flexDirection="row"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <ToggleButtonGroup
+          value={view}
+          onChange={(_, newView: VIEW) => setView(newView)}
+          color="primary"
+          exclusive
+        >
+          {Object.values(VIEW).map((viewType) => (
+            <ToggleButton key={viewType} value={viewType}>
+              {ViewButtonMap[viewType]}
+            </ToggleButton>
+          ))}
+        </ToggleButtonGroup>
 
+        <Select
+          value={language}
+          onChange={(e) => setLanguage(e.target.value as LANGUAGE)}
+          sx={{ textTransform: 'capitalize' }}
+          SelectDisplayProps={{ style: { paddingTop: 8, paddingBottom: 8 } }}
+        >
+          {Object.values(LANGUAGE).map((languageOption) => (
+            <MenuItem
+              key={languageOption}
+              value={languageOption}
+              sx={{ textTransform: 'capitalize' }}
+            >
+              {languageOption.toLowerCase()}
+            </MenuItem>
+          ))}
+        </Select>
+      </Stack>
+      <Stack
+        direction="row"
         sx={{
+          display: view === VIEW.HYBRID ? 'grid' : 'flex',
           flexDirection: 'row',
-          paddingY: 4,
-          paddingX: 8,
+          height: '100%',
+          width: '100%',
+          gridTemplateColumns: view === VIEW.HYBRID ? '1fr 2fr' : 'none',
+          gridTemplateAreas: view === VIEW.HYBRID ? "'left right'" : 'none',
         }}
       >
-        <Box
-          sx={{
-            height: '90vh',
-            width: '50%',
-            backgroundColor: 'white',
-            overflow: 'scroll'
-          }}
-        >
+        {view !== VIEW.EDITOR ? (
           <Box
             sx={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              paddingY: 1,
-              paddingX: 2,
-            }}>
-
-            <Typography
-              variant="h6"
-              color="black"
-              fontWeight="bold"
-
-              sx={{ marginBottom: 1 }}
+              height: '100%',
+              maxHeight: 'calc(100vh - 64px)',
+              overflowY: 'auto',
+              whiteSpace: 'pre-line',
+              resize: view === VIEW.HYBRID ? 'horizontal' : 'none',
+              gridArea: 'left',
+              minWidth: view === VIEW.HYBRID ? '33vw' : 'auto',
+              maxWidth: view === VIEW.HYBRID ? '67vw' : 'auto',
+              position: 'relative',
+              margin: view === VIEW.QUESTION ? 'auto' : 'none',
+            }}
+          >
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                px: 1,
+                pt: 1,
+              }}
             >
-              Two Sum
+              <Typography
+                variant="h6"
+                color="black"
+                fontWeight="bold"
+                sx={{ mb: 1, textTransform: 'capitalize' }}
+                fontSize={24}
+              >
+                {title}
+              </Typography>
+              <Typography
+                variant="h6"
+                color={DifficultyColorMap[difficulty]}
+                sx={{ textTransform: 'capitalize', mb: 1 }}
+                fontWeight="bold"
+                fontSize={24}
+              >
+                {difficulty.toLowerCase()}
+              </Typography>
+            </Box>
+            <Divider orientation="horizontal" flexItem />
+            <Typography color="black" sx={{ px: 2, my: 4 }} fontSize={18}>
+              {description}
             </Typography>
-
-            <Typography
-              variant="h6"
-              fontWeight="bold"
-              sx={{ marginBottom: 1, 
-                color: difficultyColors[difficulty] ?? 'black' }}
-            >
-              Easy
-            </Typography>
+            {(examples ?? []).map(({ input, output, explanation }, index) => (
+              <Box
+                key={`example-${index}`}
+                sx={{
+                  px: 2,
+                  my: 4,
+                }}
+              >
+                <Typography fontWeight="bold" color="black">
+                  Example {index + 1}
+                </Typography>
+                <Box
+                  color="black"
+                  sx={{
+                    backgroundColor: '#EEEDE7',
+                    borderRadius: '5px',
+                    px: 2,
+                    py: 0.5,
+                    my: 1,
+                  }}
+                >
+                  <p>
+                    <strong>Input: </strong>
+                    {input}
+                  </p>
+                  <p>
+                    <strong>Ouput: </strong>
+                    {output}
+                  </p>
+                  {explanation ? (
+                    <p>
+                      <strong>Explanation: </strong>
+                      {explanation}
+                    </p>
+                  ) : (
+                    <></>
+                  )}
+                </Box>
+              </Box>
+            ))}
+            {constraints && constraints.length ? (
+              <Typography fontWeight="bold" color="black">
+                Constraints
+              </Typography>
+            ) : (
+              <></>
+            )}
+            {(constraints ?? []).map((constraint, index) => (
+              <Typography key={`constraint-${index}`} color="black">
+                {constraint}
+              </Typography>
+            ))}
+            {view === VIEW.HYBRID ? (
+              <Typography
+                sx={{
+                  color: 'black',
+                  position: 'absolute',
+                  bottom: 0,
+                  right: 12,
+                }}
+                fontSize={12}
+              >
+                drag horizontally to resize
+              </Typography>
+            ) : (
+              <></>
+            )}
           </Box>
-          <Divider orientation='horizontal' flexItem />
-          <Typography
-            color="black"
-            sx={{ marginBottom: 2, marginLeft: 2, marginRight: 2 }}
+        ) : (
+          <></>
+        )}
+        {view !== VIEW.QUESTION ? (
+          <Box
+            sx={{
+              height: '100%',
+              width: '100%',
+              gridArea: 'right',
+            }}
           >
-            Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-            You may assume that each input would have exactly one solution, and you may not use the same element twice.
-            You can return the answer in any order.
-          </Typography>
-
-          <Typography
-            fontWeight="bold"
-            color="black"
-            sx={{ marginBottom: 2, marginLeft: 2, marginRight: 2 }}
-          >
-            Example 1
-          </Typography>
-
-          <Typography
-            color="black"
-            fontSize="14px"
-            fontFamily="Courier New"
-            sx={{ marginBottom: 2, marginLeft: 2, marginRight: 2, backgroundColor: '#EEEDE7', borderRadius: '5px' }}>
-            Input: nums = [2,7,11,15], target = 9 <br />
-            Output: [0,1] <br />
-            Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].<br />
-          </Typography>
-          <Typography
-            fontWeight="bold"
-            color="black"
-            sx={{ marginBottom: 2, marginLeft: 2, marginRight: 2 }}
-          >
-            Constraints
-          </Typography>
-
-          <Typography
-            color="black"
-            fontSize="14px"
-            sx={{ marginBottom: 2, marginLeft: 2, marginRight: 2 }}>
-            Only one valid answer exists.  <br />
-          </Typography>
-        </Box>
-
-        <Editor
-          height="90vh"
-          defaultLanguage="python"
-          defaultValue="// Type your answer here"
-        />
-
-
-
+            <Editor
+              defaultLanguage={language.toLowerCase()}
+              value={editorContent}
+              width="auto"
+              options={{
+                fontSize: '18px',
+                scrollBeyondLastLine: false,
+                minimap: { enabled: false },
+              }}
+              onChange={(newEditorContent) =>
+                setEditorContent(newEditorContent ?? '')
+              }
+            />
+          </Box>
+        ) : (
+          <></>
+        )}
       </Stack>
     </Box>
   );
