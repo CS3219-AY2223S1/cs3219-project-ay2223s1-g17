41bebc761ac2199b5595cc5a3b0a247dfc4cfa9e
myc37 <nvjn37@gmail.com>
2022-10-04 14:32:47 +0800

    fix: view rendering no longer dismounts editor

--- frontend/components/room/Room.tsx
+++ frontend/components/room/Room.tsx
@@ -63,7 +63,11 @@ const Room = () => {
           exclusive
         >
           {Object.values(VIEW).map((viewType) => (
-            <ToggleButton key={viewType} value={viewType}>
+            <ToggleButton
+              key={viewType}
+              value={viewType}
+              disabled={view === viewType}
+            >
               {ViewButtonMap[viewType]}
             </ToggleButton>
           ))}
@@ -97,139 +101,133 @@ const Room = () => {
           gridTemplateAreas: view === VIEW.HYBRID ? "'left right'" : 'none',
         }}
       >
-        {view !== VIEW.EDITOR ? (
+        <Box
+          sx={{
+            height: '100%',
+            maxHeight: 'calc(100vh - 64px)',
+            overflowY: 'auto',
+            whiteSpace: 'pre-line',
+            resize: view === VIEW.HYBRID ? 'horizontal' : 'none',
+            gridArea: 'left',
+            minWidth: view === VIEW.HYBRID ? '33vw' : 'auto',
+            maxWidth: view === VIEW.HYBRID ? '67vw' : 'auto',
+            position: 'relative',
+            margin: view === VIEW.QUESTION ? 'auto' : 'none',
+            borderRight: '1px solid rgba(0, 0, 0, 0.12)',
+            display: view === VIEW.EDITOR ? 'none' : 'inline',
+          }}
+        >
           <Box
             sx={{
-              height: '100%',
-              maxHeight: 'calc(100vh - 64px)',
-              overflowY: 'auto',
-              whiteSpace: 'pre-line',
-              resize: view === VIEW.HYBRID ? 'horizontal' : 'none',
-              gridArea: 'left',
-              minWidth: view === VIEW.HYBRID ? '33vw' : 'auto',
-              maxWidth: view === VIEW.HYBRID ? '67vw' : 'auto',
-              position: 'relative',
-              margin: view === VIEW.QUESTION ? 'auto' : 'none',
-              borderRight: '1px solid rgba(0, 0, 0, 0.12)',
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              px: 1,
+              pt: 1,
             }}
           >
+            <Typography
+              variant="h6"
+              color="black"
+              fontWeight="bold"
+              sx={{ mb: 1, textTransform: 'capitalize' }}
+              fontSize={24}
+            >
+              {title}
+            </Typography>
+            <Typography
+              variant="h6"
+              color={DifficultyColorMap[difficulty || DIFFICULTY.HARD]}
+              sx={{ textTransform: 'capitalize', mb: 1 }}
+              fontWeight="bold"
+              fontSize={24}
+            >
+              {difficulty?.toLowerCase()}
+            </Typography>
+          </Box>
+          <Divider orientation="horizontal" flexItem />
+          <Typography color="black" sx={{ px: 2, my: 4 }} fontSize={18}>
+            {description}
+          </Typography>
+          {(examples ?? []).map(({ input, output, explanation }, index) => (
             <Box
+              key={`example-${index}`}
               sx={{
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                px: 1,
-                pt: 1,
+                px: 2,
+                my: 4,
               }}
             >
-              <Typography
-                variant="h6"
-                color="black"
-                fontWeight="bold"
-                sx={{ mb: 1, textTransform: 'capitalize' }}
-                fontSize={24}
-              >
-                {title}
-              </Typography>
-              <Typography
-                variant="h6"
-                color={DifficultyColorMap[difficulty || DIFFICULTY.HARD]}
-                sx={{ textTransform: 'capitalize', mb: 1 }}
-                fontWeight="bold"
-                fontSize={24}
-              >
-                {difficulty?.toLowerCase()}
-              </Typography>
-            </Box>
-            <Divider orientation="horizontal" flexItem />
-            <Typography color="black" sx={{ px: 2, my: 4 }} fontSize={18}>
-              {description}
-            </Typography>
-            {(examples ?? []).map(({ input, output, explanation }, index) => (
-              <Box
-                key={`example-${index}`}
-                sx={{
-                  px: 2,
-                  my: 4,
-                }}
-              >
-                <Typography fontWeight="bold" color="black">
-                  Example {index + 1}
-                </Typography>
-                <Box
-                  color="black"
-                  sx={{
-                    backgroundColor: '#EEEDE7',
-                    borderRadius: '5px',
-                    px: 2,
-                    py: 0.5,
-                    my: 1,
-                  }}
-                >
-                  <p>
-                    <strong>Input: </strong>
-                    {input}
-                  </p>
-                  <p>
-                    <strong>Ouput: </strong>
-                    {output}
-                  </p>
-                  {explanation ? (
-                    <p>
-                      <strong>Explanation: </strong>
-                      {explanation}
-                    </p>
-                  ) : (
-                    <></>
-                  )}
-                </Box>
-              </Box>
-            ))}
-            {constraints && constraints.length ? (
               <Typography fontWeight="bold" color="black">
-                Constraints
+                Example {index + 1}
               </Typography>
-            ) : (
-              <></>
-            )}
-            {(constraints ?? []).map((constraint, index) => (
-              <Typography key={`constraint-${index}`} color="black">
-                {constraint}
-              </Typography>
-            ))}
-            {view === VIEW.HYBRID ? (
-              <Typography
+              <Box
+                color="black"
                 sx={{
-                  position: 'absolute',
-                  bottom: 0,
-                  right: 12,
+                  backgroundColor: '#EEEDE7',
+                  borderRadius: '5px',
+                  px: 2,
+                  py: 0.5,
+                  my: 1,
                 }}
-                color="rgba(0,0,0,0.7)"
-                fontSize={12}
               >
-                drag horizontally to resize
-              </Typography>
-            ) : (
-              <></>
-            )}
-          </Box>
-        ) : (
-          <></>
-        )}
-        {view !== VIEW.QUESTION ? (
-          <Box
-            sx={{
-              height: '100%',
-              width: '100%',
-              gridArea: 'right',
-            }}
-          >
-            <CodeEditor language={language} templates={templates} />
-          </Box>
-        ) : (
-          <></>
-        )}
+                <p>
+                  <strong>Input: </strong>
+                  {input}
+                </p>
+                <p>
+                  <strong>Ouput: </strong>
+                  {output}
+                </p>
+                {explanation ? (
+                  <p>
+                    <strong>Explanation: </strong>
+                    {explanation}
+                  </p>
+                ) : (
+                  <></>
+                )}
+              </Box>
+            </Box>
+          ))}
+          {constraints && constraints.length ? (
+            <Typography fontWeight="bold" color="black">
+              Constraints
+            </Typography>
+          ) : (
+            <></>
+          )}
+          {(constraints ?? []).map((constraint, index) => (
+            <Typography key={`constraint-${index}`} color="black">
+              {constraint}
+            </Typography>
+          ))}
+          {view === VIEW.HYBRID ? (
+            <Typography
+              sx={{
+                position: 'absolute',
+                bottom: 0,
+                right: 12,
+              }}
+              color="rgba(0,0,0,0.7)"
+              fontSize={12}
+            >
+              drag horizontally to resize
+            </Typography>
+          ) : (
+            <></>
+          )}
+        </Box>
+        <Box
+          sx={{
+            height: '100%',
+            width: '100%',
+            gridArea: 'right',
+            display: view === VIEW.QUESTION ? 'none' : 'inline',
+          }}
+        >
+          <CodeEditor language={language} templates={templates} />
+        </Box>
       </Stack>
     </Box>
   );
