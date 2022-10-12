1483290497343deeeb86110cae8ec52a42934793
myc37 <nvjn37@gmail.com>
2022-10-12 21:48:01 +0800

    chore: remove stopwatch logic from matching

--- backend/matching-service/src/timers.ts
+++ backend/matching-service/src/timers.ts
@@ -1,19 +1,3 @@
 type TimerId = string;
 type Timers = Record<TimerId, NodeJS.Timer>;
-export type Time = {
-  hours: number;
-  minutes: number;
-  seconds: number;
-};
-type Stopwatches = Record<
-  TimerId,
-  {
-    interval: NodeJS.Timer | undefined;
-    time: Time;
-    isActive: boolean;
-    isPaused: boolean;
-  }
->;
-
 export const timers: Timers = {};
-export const stopwatches: Stopwatches = {};
