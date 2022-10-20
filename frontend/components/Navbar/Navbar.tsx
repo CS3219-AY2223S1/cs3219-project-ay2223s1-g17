26b767ac398de37c8ba49b63223c989d06cd4c54
myc37 <nvjn37@gmail.com>
2022-10-20 18:00:24 +0800

    feat: add nav icons and add account option

--- frontend/components/Navbar/Navbar.tsx
+++ frontend/components/Navbar/Navbar.tsx
@@ -4,6 +4,8 @@ import useAuth from 'contexts/AuthContext';
 import { NAVBAR_HEIGHT_PX } from 'utils/constants';
 import { Login, Logout } from '@mui/icons-material';
 import { Button, SxProps, Theme } from '@mui/material';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import JoinInnerIcon from '@mui/icons-material/JoinInner';
 
 const Navbar = () => {
   const { user, logout } = useAuth();
@@ -15,7 +17,7 @@ const Navbar = () => {
         width: '100%',
         height: `${NAVBAR_HEIGHT_PX}px`,
         position: 'fixed',
-        background: 'linear-gradient(45deg, #2370C8, #BFFFBE)',
+        backgroundColor: 'inherit',
         top: 0,
         left: 0,
         zIndex: 1,
@@ -30,22 +32,25 @@ const Navbar = () => {
         // eslint-disable-next-line jsx-a11y/alt-text, @next/next/no-img-element
         icon={<img src={'/assets/logo.png'} style={{ height: '44px' }}></img>}
         styles={{
-          color: '#FFFFFF',
           fontFamily: 'Raleway',
           fontSize: 28,
           fontWeight: 'bold',
         }}
+        variant="text"
+        disableHoverEffect
       />
       {user ? (
         <span style={{ display: 'flex', columnGap: 8 }}>
           <NavBarOption
             onClick={() => router.push('/match')}
+            icon={<JoinInnerIcon />}
             label="Find Match"
             styles={{ fontFamily: 'Raleway' }}
           />
           <NavBarOption
-            onClick={() => router.push('/editor')}
-            label="Try Code Editor"
+            onClick={() => router.push('/auth')}
+            icon={<ManageAccountsIcon />}
+            label="Account"
             styles={{ fontFamily: 'Raleway' }}
           />
           <NavBarOption
@@ -81,17 +86,37 @@ const Navbar = () => {
 
 type Props = {
   onClick: MouseEventHandler<HTMLButtonElement>;
-  icon?: ReactElement;
   label: string;
+  icon?: ReactElement;
   styles?: SxProps<Theme>;
+  variant?: 'contained' | 'text' | 'outlined';
+  disableHoverEffect?: boolean;
 };
 
-const NavBarOption: FC<Props> = ({ onClick, icon, label, styles }) => {
+const NavBarOption: FC<Props> = ({
+  onClick,
+  icon,
+  label,
+  styles,
+  disableHoverEffect,
+  variant = 'contained',
+}) => {
   return (
     <Button
       startIcon={icon}
       onClick={onClick}
-      sx={{ textTransform: 'none', color: 'black', ...styles }}
+      sx={{
+        textTransform: 'none',
+        ...styles,
+        ...(disableHoverEffect
+          ? {
+              '&.MuiButtonBase-root:hover': {
+                bgcolor: 'transparent',
+              },
+            }
+          : {}),
+      }}
+      variant={variant}
     >
       {label}
     </Button>
