771b7f28bbdb3428805cccffda01d1989d384939
Dominic Lim <dominiclimkj3448@gmail.com>
2022-10-22 22:28:17 +0800

    fix: wrong attribute name

--- backend/question-service/src/data/seed.ts
+++ backend/question-service/src/data/seed.ts
@@ -45,11 +45,11 @@ export default [
       'Given the head of a singly linked list, reverse the list, and return the reversed list.',
     topics: [
       {
-        skills: 'INTERMEDIATE',
+        skill: 'INTERMEDIATE',
         topics: ['RECURSION'],
       },
       {
-        skills: 'FUNDAMENTAL',
+        skill: 'FUNDAMENTAL',
         topics: ['LINKED LIST'],
       },
     ],
@@ -83,11 +83,11 @@ export default [
       'You are given the heads of two sorted linked lists list1 and list2.\n\nMerge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.\n\nReturn the head of the merged linked list.',
     topics: [
       {
-        skills: 'INTERMEDIATE',
+        skill: 'INTERMEDIATE',
         topics: ['RECURSION'],
       },
       {
-        skills: 'FUNDAMENTAL',
+        skill: 'FUNDAMENTAL',
         topics: ['LINKED LIST'],
       },
     ],
@@ -121,7 +121,7 @@ export default [
       "Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.\n\nAn input string is valid if:\n\n1. Open brackets must be closed by the same type of brackets.\n\n2. Open brackets must be closed in the correct order.\n\n3. Every close bracket has a corresponding open bracket of the same type.",
     topics: [
       {
-        skills: 'FUNDAMENTAL',
+        skill: 'FUNDAMENTAL',
         topics: ['STACK', 'STRING'],
       },
     ],
@@ -155,11 +155,11 @@ export default [
       'You are climbing a staircase. It takes n steps to reach the top.\n\nEach time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?',
     topics: [
       {
-        skills: 'ADVANCED',
+        skill: 'ADVANCED',
         topics: ['DYNAMIC PROGRAMMING', 'MEMOIZATION'],
       },
       {
-        skills: 'INTERMEDIATE',
+        skill: 'INTERMEDIATE',
         topics: ['MATH'],
       },
     ],
@@ -193,7 +193,7 @@ export default [
       "A matrix diagonal is a diagonal line of cells starting from some cell in either the topmost row or leftmost column and going in the bottom-right direction until reaching the matrix's end. For example, the matrix diagonal starting from mat[2][0], where mat is a 6 x 3 matrix, includes cells mat[2][0], mat[3][1], and mat[4][2].\n\nGiven an m x n matrix mat of integers, sort each matrix diagonal in ascending order and return the resulting matrix.",
     topics: [
       {
-        skills: 'FUNDAMENTAL',
+        skill: 'FUNDAMENTAL',
         topics: ['ARRAY', 'MATRIX', 'SORTING'],
       },
     ],
@@ -225,11 +225,11 @@ export default [
       'Given a string s, return the longest palindromic substring in s.\n\nA string is called a palindrome string if the reverse of that string is the same as the original string.',
     topics: [
       {
-        skills: 'ADVANCED',
+        skill: 'ADVANCED',
         topics: ['DYNAMIC PROGRAMMING'],
       },
       {
-        skills: 'FUNDAMENTAL',
+        skill: 'FUNDAMENTAL',
         topics: ['STRING'],
       },
     ],
@@ -260,7 +260,7 @@ export default [
       'Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.\n\nNotice that the solution set must not contain duplicate triplets.',
     topics: [
       {
-        skills: 'FUNDAMENTAL',
+        skill: 'FUNDAMENTAL',
         topics: ['ARRAY', 'SORTING', 'TWO POINTERS'],
       },
     ],
@@ -298,11 +298,11 @@ export default [
       "Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.",
     topics: [
       {
-        skills: 'ADVANCED',
+        skill: 'ADVANCED',
         topics: ['DYNAMIC PROGRAMMING'],
       },
       {
-        skills: 'FUNDAMENTAL',
+        skill: 'FUNDAMENTAL',
         topics: ['STACK', 'STRING'],
       },
     ],
@@ -338,11 +338,11 @@ export default [
       'There are n children standing in a line. Each child is assigned a rating value given in the integer array ratings.\n\nYou are giving candies to these children subjected to the following requirements:\n\nEach child must have at least one candy.\n\nChildren with a higher rating get more candies than their neighbors.\n\nReturn the minimum number of candies you need to have to distribute the candies to the children.',
     topics: [
       {
-        skills: 'INTERMEDIATE',
+        skill: 'INTERMEDIATE',
         topics: ['GREEDY'],
       },
       {
-        skills: 'FUNDAMENTAL',
+        skill: 'FUNDAMENTAL',
         topics: ['ARRAY'],
       },
     ],
