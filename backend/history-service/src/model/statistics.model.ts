889c06a362e2dd77a3252ab6f3b7e00738a70f98
myc37 <nvjn37@gmail.com>
2022-10-24 12:29:23 +0800

    feat: add statistics

---
+++ backend/history-service/src/model/statistics.model.ts
@@ -0,0 +1,124 @@
+import { model, Schema } from 'mongoose';
+import { HttpStatusCode, PeerPrepError } from '../../../utils';
+import { IStatistics, IStatisticsModel, Question } from './statistics.types';
+
+const statisticsSchema = new Schema<IStatistics, IStatisticsModel>({
+  user: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  completedQuestions: [
+    {
+      type: String,
+      default: [],
+    },
+  ],
+  completedQuestionsByDifficulty: {
+    type: Map,
+    of: Number,
+    default: new Map<string, number>(),
+  },
+  languagesUsed: {
+    type: Map,
+    of: Number,
+    default: new Map<string, number>(),
+  },
+  completedQuestionsByDay: {
+    type: Map,
+    of: Number,
+    default: new Map<string, number>(),
+  },
+  completedTopics: {
+    type: Map,
+    of: Number,
+    default: new Map<string, number>(),
+  },
+  dailyStreak: {
+    type: Number,
+    default: 0,
+  },
+});
+
+statisticsSchema.pre('save', async function (callback) {
+  const statistics = this;
+
+  if (!statistics.isNew && statistics.isModified('languagesUsed')) {
+    const now = new Date();
+    const startOfYear = new Date(now.getFullYear(), 0, 0);
+    const timeSinceStartOfYear = now.getTime() - startOfYear.getTime();
+    const oneDayInMilliseconds = 1000 * 60 * 60 * 24;
+    const dayOfYear = String(
+      Math.floor(timeSinceStartOfYear / oneDayInMilliseconds)
+    );
+
+    const completedQuestionsToday =
+      statistics.completedQuestionsByDay.get(dayOfYear) ?? 0;
+    const completedQuestionsYesterday =
+      statistics.completedQuestionsByDay.get(String(parseInt(dayOfYear) - 1)) ??
+      0;
+
+    if (completedQuestionsYesterday && !completedQuestionsToday)
+      statistics.dailyStreak += 1;
+    statistics.completedQuestionsByDay.set(
+      dayOfYear,
+      completedQuestionsToday + 1
+    );
+  }
+
+  callback();
+});
+
+statisticsSchema.static(
+  'getStatisticsByUser',
+  async function getStatisticsByUser(userId: string) {
+    if (!userId) throw new Error('User id is required');
+
+    const statistics = Statistics.findOne({ user: userId }).sort({
+      languagesUsed: 'desc',
+    });
+    if (!statistics)
+      throw new PeerPrepError(
+        HttpStatusCode.NOT_FOUND,
+        'No statistics found for this user'
+      );
+
+    return statistics;
+  }
+);
+
+statisticsSchema.static(
+  'updateStatisticsByUser',
+  async function updateStatisticsByUser(
+    userId: string,
+    completedQuestion: Question
+  ) {
+    if (!userId) throw new Error('User id is required');
+
+    const statistics = await Statistics.getStatisticsByUser(userId);
+
+    const { id, difficulty, language } = completedQuestion;
+
+    statistics.languagesUsed.set(
+      language,
+      (statistics.languagesUsed.get(language) ?? 0) + 1
+    );
+
+    if (!statistics.completedQuestions.includes(id)) {
+      statistics.completedQuestions.push(id);
+      statistics.completedQuestionsByDifficulty.set(
+        difficulty,
+        (statistics.completedQuestionsByDifficulty.get(difficulty) ?? 0) + 1
+      );
+    }
+
+    await statistics.save();
+  }
+);
+
+const Statistics = model<IStatistics, IStatisticsModel>(
+  'Statistics',
+  statisticsSchema
+);
+
+export default Statistics;
---
+++ backend/history-service/src/model/statistics.types.ts
@@ -0,0 +1,29 @@
+import { Document, Model, Types } from 'mongoose';
+import { LANGUAGE, DIFFICULTY } from '../../../utils';
+
+export type Question = {
+  id: string;
+  language: LANGUAGE;
+  difficulty: DIFFICULTY;
+};
+
+export interface IStatistics {
+  user: string;
+  completedQuestions: string[];
+  completedQuestionsByDifficulty: Map<string, number>;
+  completedTopics: Map<string, number>;
+  completedQuestionsByDay: Map<string, number>;
+  languagesUsed: Map<string, number>;
+  dailyStreak: number;
+}
+
+export interface IStatisticsMethods {}
+
+export type StatisticsDocument = Document<IStatistics> &
+  IStatistics & { _id: Types.ObjectId } & IStatisticsMethods;
+
+export interface IStatisticsModel
+  extends Model<IStatistics, {}, IStatisticsMethods> {
+  getStatisticsByUser(userId: string): Promise<StatisticsDocument>;
+  updateStatisticsByUser(userId: string, question: Question): Promise<void>;
+}
