cd3af3fb0f91ac46cb6aa74206dbf17947b8ce03
myc37 <nvjn37@gmail.com>
2022-10-24 12:33:29 +0800

    feat: add loading page

---
+++ frontend/components/LoadingPage/LoadingPage.tsx
@@ -0,0 +1,24 @@
+import { Stack, CircularProgress, Typography } from '@mui/material';
+import PageWrapper from 'components/PageWrapper';
+import React from 'react';
+
+const LoadingPage = () => {
+  return (
+    <PageWrapper>
+      <Stack
+        sx={{
+          width: '100%',
+          height: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        rowGap={2}
+      >
+        <CircularProgress size={100} />
+        <Typography>Loading...</Typography>
+      </Stack>
+    </PageWrapper>
+  );
+};
+
+export default LoadingPage;
