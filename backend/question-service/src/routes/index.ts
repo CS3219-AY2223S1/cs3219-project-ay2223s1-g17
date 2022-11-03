f4803db98e95ff100ffda30eabf05cfee2a3f193
Dominic Lim <dominiclimkj3448@gmail.com>
2022-11-04 01:43:05 +0800

    fix: bug with single end point routes

--- backend/question-service/src/routes/index.ts
+++ backend/question-service/src/routes/index.ts
@@ -13,10 +13,10 @@ router.get('/', (_: Request, res: Response) => {
   res.status(200).send('<h1>Question Service</h1>');
 });
 
-router.route('/:id').get(getQuestion);
 router.route('/difficulty/:difficulty').get(getQuestionByDifficulty);
 router.route('/all').get(getQuestions);
 router.route('/count').get(getQuestionsCount);
+router.route('/:id').get(getQuestion);
 router.route('/seed').post(loadQuestions);
 
 export default router;
