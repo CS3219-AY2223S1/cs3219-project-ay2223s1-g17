4365c8456c03abbef3c4eeb966cebb152953bb7e
myc37 <nvjn37@gmail.com>
2022-11-07 19:28:39 +0800

    fix: route login button to login mode

--- frontend/components/AuthForm/AuthForm.tsx
+++ frontend/components/AuthForm/AuthForm.tsx
@@ -65,7 +65,9 @@ const AuthForm = () => {
 
   useEffect(() => {
     const { mode } = router.query;
-    if (mode === 'register') setIsLogin(false);
+    setIsLogin((prev) =>
+      mode === 'register' ? false : mode === 'login' ? true : prev
+    );
   }, [router]);
 
   const handleOpen = () => {
@@ -155,7 +157,12 @@ const AuthForm = () => {
     <>
       <Stack
         spacing={2}
-        sx={{ justifyContent: 'center', alignItems: 'center', height: '100%', mt: '4rem' }}
+        sx={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100%',
+          mt: '4rem',
+        }}
       >
         <form onSubmit={handleSubmit}>
           <Stack
